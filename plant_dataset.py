267935d6eead37374e3d8b21161f64af37126d05
Arif <arif@themascoteers.com>
2024-02-13 22:46:00 +1100

    done

--- plant_dataset.py
+++ plant_dataset.py
@@ -1,10 +1,6 @@
 import torch
 from torch.utils.data import Dataset
 import pandas as pd
-import numpy as np
-import os
-from torchvision import transforms
-from PIL import Image
 
 
 class PlantDataset(Dataset):
@@ -12,50 +8,35 @@ class PlantDataset(Dataset):
         self.is_train = 0
         if is_train:
             self.is_train = 1
-        df = pd.read_csv("data/info_modified.csv")
+        df = pd.read_csv("data/info.csv")
         df = df[df["is_train"] == self.is_train]
-        self.image_files = list(df["File.Name"])
-        self.group = list(df["Group"])
-        self.label_mapping = {"S":0, "T":1}
+
+        self.label_mapping = {"S": 0, "T": 1}
+        self.group = []
+        for group in list(df["Group"]):
+            self.group.append(self.label_mapping[group])
+        self.group = torch.tensor(self.group)
+
+        columns_to_drop = ['File.Name', 'Genotype ', 'Treatment', 'Replication', 'Group', 'Day']
+        df = df.drop(columns=columns_to_drop, axis=1)
+        self.data = torch.tensor(df.to_numpy(), dtype=torch.float32)
 
     def __len__(self):
         return len(self.group)
 
     def __getitem__(self, idx):
         group = self.group[idx]
-        group = self.label_mapping[group]
-        group = torch.tensor(group)
-        image_file = self.image_files[idx]
-        image_file = image_file + ".png"
-
-        image_path = f"data/images/{image_file}"
-        transform = transforms.Compose([
-            transforms.Resize((125, 75)),
-            transforms.ToTensor(),
-            transforms.Normalize([0.5], [0.5])
-        ])
-        image = Image.open(image_path)
-        tensor_image = transform(image)
-        return tensor_image, group
+        data = self.data[idx]
+        return data, group
 
 
 if __name__ == "__main__":
     from torch.utils.data import DataLoader
-    import matplotlib.pyplot as plt
 
     ds = PlantDataset(is_train=True)
-    dl = DataLoader(ds, batch_size=20, shuffle=True)
+    dl = DataLoader(ds, batch_size=1000, shuffle=True)
 
-    for image, group in dl:
-        print(image.shape)
+    for data, group in dl:
+        print(data.shape)
         print(group.shape)
-
-        image = image[0]
-        tensor_image_display = image.squeeze().numpy()
-        tensor_image_display = (tensor_image_display + 1) / 2.0
-
-        plt.imshow(tensor_image_display, cmap='gray')
-        plt.axis('off')
-        plt.show()
-
         break
