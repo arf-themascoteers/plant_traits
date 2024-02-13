6db012470dbf48373432d01aab2018c4a20e08e4
Arif <arif@themascoteers.com>
2024-02-13 22:52:18 +1100

    done

--- train.py
+++ train.py
@@ -14,14 +14,13 @@ def train():
     ds = PlantDataset(is_train=True)
     dl = DataLoader(ds, batch_size=100, shuffle=True)
     loss = 0
-    for epoch in range(100):
+    for epoch in range(10000):
         for inputs, labels in dl:
             optimizer.zero_grad()
             outputs = model(inputs)
             loss = criterion(outputs, labels)
             loss.backward()
             optimizer.step()
-            print(f"loss: {loss.item()}")
         print(f"After {epoch+1} epoch, loss: {loss.item()}")
     torch.save(model.state_dict(), 'saved.pth')
 
